51aa4dae1f6e2a32e834d1dfbe6c07489ff2fa07
qubitnumber <qubitnumber@gmail.com>
2023-12-28 05:06:02 -0500

    fix ref

--- models/prompt.js
+++ models/prompt.js
@@ -3,7 +3,7 @@ import { Schema, model, models } from 'mongoose';
 const promptSchema = new Schema({
   creator: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
+    ref: 'users',
   },
   prompt: {
     type: String,
