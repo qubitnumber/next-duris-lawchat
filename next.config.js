5c2e7212d8d2ddc1b8a5067ed44335bbcd124028
qubitnumber <qubitnumber@gmail.com>
2023-12-28 00:19:45 -0500

    Edit next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  experimental: {
+    esmExternals: "loose",
+    serverComponentsExternalPackages: ["mongoose"]
+  },
 }
 
 module.exports = nextConfig
