832da281189cffb8e03aa812ba952a928e55e488
qubitnumber <qubitnumber@gmail.com>
2023-12-29 15:33:41 -0500

    A

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -26,12 +26,7 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch('/api/prompt', {
-        method: 'GET',
-        headers:{
-          'Content-Type':'application/json'
-        },
-    });
+    const response = await fetch('/api/prompt');
     const data = await response.json();
 
     setAllPosts(data);
