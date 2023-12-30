ea1e42e871181cba89959fecdacdc677cdadf18d
qubitnumber <qubitnumber@gmail.com>
2023-12-30 06:49:59 -0500

    A

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,9 @@ const Feed = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch('/api/prompt');
+      const response = await fetch('/api/prompt', {
+        method: "POST",
+      });
       const data = await response.json();
   
       setAllPosts(data);
